a12d646d962dda15ea68129fe50f71260dd14c5d
tollispa <112690202+tollispa@users.noreply.github.com>
2023-05-13 10:48:07 +0200

    imported comps in home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import {TextField} from "@mui/material"
+import FoodCategory from "./FoodCategory";
+import Footer from "./Footer"
 
 function Home() {
     return ( 
+        <>
         <div 
         className="max-w-[1240px]' rounded h-[300px] sm:h-[600px] bg-cover bg-center items-center justify-center hidden sm:flex"
         style={{backgroundImage: `url("https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/4233505/1820/1213/m1/fpnw/wm1/th6dcidge1e8u2cjh7qznbf7o7fefd3wmb0exr1zupcsmvydd0suh6auhmmxbv0h-.jpg?1522764671&s=e38e80deeed0ccefdc17071a21afb43c")`}}>
@@ -12,7 +15,11 @@ function Home() {
                 <p className="pt-4 ml-3 mr-3">Eller</p>
                 <button className="bg-red-400 rounded text-white p-2 mr-1 ml-1 hover:bg-red-500 w-[120px] ">Pick-Up</button>
             </div>
+          
         </div>
+          <FoodCategory/>
+          <Footer/>
+          </>
     );
 }
 

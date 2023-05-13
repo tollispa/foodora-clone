9364df969821a2a1dcc7c3bd16819f824c7eb9f6
tollispa <112690202+tollispa@users.noreply.github.com>
2023-05-13 10:47:51 +0200

    added array of food icons export

---
+++ src/components/foodIcons.js
@@ -0,0 +1,24 @@
+
+import { FaAppleAlt, FaBacon, FaBreadSlice, FaCarrot, FaCheese, FaCoffee, FaCookie, FaDrumstickBite, FaEgg, FaFish, FaHamburger, FaHotdog, FaIceCream, FaLemon, FaPepperHot, FaPizzaSlice, } from 'react-icons/fa';
+
+export const foodIcons = [
+    { title: "Apple", price: 10, icon: <FaAppleAlt /> },
+    { title: "Bacon", price: 20, icon: <FaBacon /> },
+    { title: "Bread Slice", price: 15, icon: <FaBreadSlice /> },
+    { title: "Carrot", price: 5, icon: <FaCarrot /> },
+    { title: "Cheese", price: 33, icon: <FaCheese /> },
+    { title: "Coffee", price: 17, icon: <FaCoffee /> },
+    { title: "Cookie", price: 10, icon: <FaCookie /> },
+    { title: "Drumstick", price: 7, icon: <FaDrumstickBite /> },
+    { title: "Egg", price: 25, icon: <FaEgg /> },
+    { title: "Fish", price: 33, icon: <FaFish /> },
+    { title: "Hamburger", price: 45, icon: <FaHamburger /> },
+    { title: "Hotdog", price: 32, icon: <FaHotdog /> },
+    { title: "Ice Cream", price: 13, icon: <FaIceCream /> },
+    { title: "Lemon", price: 5, icon: <FaLemon /> },
+    { title: "Pepper Hot", price: 2, icon: <FaPepperHot /> },
+    { title: "Pizza Slice", price: 40, icon: <FaPizzaSlice /> },
+  ];
+
+  export const cartItems = []
+  export const price = 0
